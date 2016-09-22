c2da514a0be80abf6d9a54367cda3899467f8596
Kirill Melnikov <kir_melnikov@skbkontur.ru>
2016-09-22 19:29:15 +0500

    Transition commit

--- cagrr.go
+++ cagrr.go
@@ -1,9 +1,13 @@
 package cagrr
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/big"
+	"net/http"
 	"os/exec"
-	"strconv"
 
 	s "strings"
 )
@@ -34,6 +38,7 @@ type Token struct {
 	Node *Node
 	ID   int64
 	Next int64
+	Host string
 }
 
 // Fragment of Token range for repair
@@ -44,55 +49,86 @@ type Fragment struct {
 	Finish int64
 }
 
-// RepairResult keeps information about Fragment repair status
-type RepairResult struct {
-	Frag    *Fragment
-	Message string
-	Error   error
+// Repair is a Unit of repair work
+type Repair struct {
+	ID       int64             `json:"id"`
+	Keyspace string            `json:"keyspace"`
+	Cause    string            `json:"cause"`
+	Owner    string            `json:"owner"`
+	Options  map[string]string `json:"options"`
+	Callback string            `json:"callback"`
+	Message  string            `json:"message"`
+}
+
+// RepairStatus keeps status of repair
+type RepairStatus struct {
+	ID      int64  `json:"id"`
+	Message string `json:"message"`
+	Error   bool   `json:"error"`
+	Type    string `json:"type"`
+	Count   int    `json:"count"`
+	Total   int    `json:"total"`
+}
+
+func (n Node) Ranges(slices int) ([]string, error) {
+	url := fmt.Sprintf("http://%s:%d/ring/%d", n.Host, n.Port, slices)
+	r, _ := http.Get(url)
+	response, _ := ioutil.ReadAll(r.Body)
+	var ranges []string
+
+	err := json.Unmarshal(response, &ranges)
+	return ranges, err
 }
 
 // Tokens initializes array of node tokens
 func (n Node) Tokens() (map[int64]Token, []int64) {
-	//url := fmt.Sprintf("http://%s:%d", n.Host, n.Port)
-	//runner := NewRunner(url)
-	//return runner.Ring()
-	result := make(map[int64]Token)
-	args := []string{
-		"-h", n.Host,
-		"-p", fmt.Sprintf("%d", n.Port),
-		"ring"}
-	res, err := nodetool(args)
-	if err != nil {
-		panic(err)
-	}
-	lines := s.Split(res, "\n")
+	url := fmt.Sprintf("http://%s:%d/ring", n.Host, n.Port)
+	r, _ := http.Get(url)
+	response, _ := ioutil.ReadAll(r.Body)
+	var ring map[int64]string
+	var result = make(map[int64]Token)
+
+	err := json.Unmarshal(response, &ring)
 	var prev int64
 	var keys []int64
-	for _, str := range lines {
-		fields := s.Fields(str)
-		if len(fields) == numFields {
-			i, err := strconv.ParseInt(fields[7], 10, 64)
-			if err != nil {
-				continue
-			}
+	if err != nil {
+		for i, host := range ring {
 			keys = append(keys, i)
 			next := i
 			if prev == 0 {
 				prev = i
 				continue
 			}
-			token := Token{Node: &n, ID: prev, Next: next}
+			token := Token{Node: &n, ID: prev, Next: next, Host: host}
 
 			result[prev] = token
 			prev = next
 		}
 	}
 	return result, keys
-
 }
 
 // Fragments is a set of ranges in Token
 func (t Token) Fragments(steps int) []Fragment {
+	var min *big.Int
+	var max *big.Int
+	var size *big.Int
+	one := big.NewInt(1)
+	two := big.NewInt(2)
+	power := big.NewInt(63)
+
+	min = min.Exp(two, power, nil)
+	min = min.Neg(min)
+
+	max = max.Exp(two, power, nil)
+	max = max.Sub(max, one)
+
+	size = size.Sub(max, min)
+	size = size.Add(size, one)
+	//BigInteger RANGE_MIN = new BigInteger("2").pow(63).negate();
+	//RANGE_MAX = new BigInteger("2").pow(63).subtract(BigInteger.ONE);
+	//RANGE_SIZE = RANGE_MAX.subtract(RANGE_MIN).add(BigInteger.ONE);
+
 	var result []Fragment
 	step := (t.Next - t.ID) / int64(steps)
 	for i := t.ID; i < t.Next; i += step {
@@ -103,32 +139,38 @@ func (t Token) Fragments(steps int) []Fragment {
 		default:
 			finish = i + step
 		}
-		frag := Fragment{Token: &t, Start: i, Finish: finish}
+		frag := Fragment{Token: &t, Start: i, Finish: finish - 1}
 		result = append(result, frag)
 	}
 	return result
 }
 
 // Repair fragment of node range
-func (f Fragment) Repair(keyspace string) (RepairResult, error) {
-	//runner := NewRunner(jolokiaURL, domainName)
+func (f Fragment) Repair(keyspace string, cause string, owner string, callback string) (Repair, error) {
 
-	node := f.Token.Node
-	args := []string{
-		"-h", node.Host,
-		"-p", fmt.Sprintf("%d", node.Port),
-		"repair", keyspace,
-		"-st", fmt.Sprintf("%d", f.Start),
-		"-et", fmt.Sprintf("%d", f.Finish)}
-
-	str, err := nodetool(args)
-
-	result := RepairResult{
-		Frag:    &f,
-		Message: str,
-		Error:   err,
+	options := &Repair{
+		Keyspace: keyspace,
+		Cause:    cause,
+		Owner:    owner,
+		Callback: callback,
+		Options: map[string]string{
+			"parallelism": "parallel",
+			"ranges":      fmt.Sprintf("%d:%d", f.Start, f.Finish-1),
+		},
 	}
-	return result, err
+
+	url := fmt.Sprintf("http://%s:%d/repair", f.Token.Node.Host, f.Token.Node.Port)
+
+	buf, _ := json.Marshal(options)
+	body := bytes.NewBuffer(buf)
+	r, err := http.Post(url, "application/json", body)
+	var repair Repair
+	if err == nil {
+		response, _ := ioutil.ReadAll(r.Body)
+		err = json.Unmarshal(response, &repair)
+	}
+
+	return repair, err
 }
 
 func nodetool(args []string) (string, error) {
--- cmd/cajrr/main.go
+++ cmd/cajrr/main.go
@@ -5,39 +5,47 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
+	"net"
 	"net/http"
+	"os"
+
+	elastic "gopkg.in/olivere/elastic.v3"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/go-kit/kit/metrics/graphite"
+	flags "github.com/jessevdk/go-flags"
+	metrics "github.com/rcrowley/go-metrics"
+	"github.com/skbkontur/cagrr"
+	"github.com/sohlich/elogrus"
 )
 
-// Repair is a Unit of repair work
-type Repair struct {
-	ID       int64             `json:"id"`
-	Keyspace string            `json:"keyspace"`
-	Cause    string            `json:"cause"`
-	Owner    string            `json:"owner"`
-	Options  map[string]string `json:"options"`
-	Callback string            `json:"callback"`
-	Message  string            `json:"message"`
+var opts struct {
+	Host      string `short:"h" long:"host" default:"localhost" description:"Address of CAJRR service" env:"CAJRR_HOST"`
+	Port      int    `short:"p" long:"port" default:"8080" description:"CAJRR port" env:"CAJRR_PORT"`
+	Keyspace  string `short:"k" long:"keyspace" default:"*" description:"Keyspace to repair" env:"REPAIR_KEYSPACE"`
+	Steps     int    `short:"s" long:"steps" default:"1" description:"Steps to split token ranges to" env:"REPAIR_STEPS"`
+	Workers   int    `short:"w" long:"workers" default:"1" description:"Number of concurrent workers" env:"REPAIR_WORKERS"`
+	Verbosity string `short:"v" long:"verbosity" description:"Verbosity of tool, possible values are: panic, fatal, error, waring, debug" env:"REPAIR_VERBOSITY"`
 }
 
-// RepairStatus keeps status of repair
-type RepairStatus struct {
-	ID      int64  `json:"id"`
-	Message string `json:"message"`
-	Error   bool   `json:"error"`
-	Type    string `json:"type"`
-	Count   int    `json:"count"`
-	Total   int    `json:"total"`
+// Metrics of repair process
+type Metrics struct {
+	fragmentCount metrics.Counter
+	errorCount    metrics.Counter
+	repairTime    metrics.Meter
 }
 
+var instr Metrics
+var int count
+
+// check progress
 func repairStatus(w http.ResponseWriter, req *http.Request) {
 	body, _ := ioutil.ReadAll(req.Body)
-	var status RepairStatus
+	var status cagrr.RepairStatus
 	err := json.Unmarshal(body, &status)
 	if err == nil {
 		percent := status.Count * 100 / status.Total
 		fmt.Printf("\r%d/%d=%d%%", status.Count, status.Total, percent)
-
 	} else {
 		fmt.Println(err)
 	}
@@ -50,28 +58,87 @@ func runServer() {
 	}
 }
 
-func main() {
-	go runServer()
+func repair(node cagrr.Node) {
 
-	options := &Repair{
+	w := opts.Workers
+	jobs := make(chan cagrr.Fragment, w)
+	results := make(chan cagrr.RepairResult, w)
+
+	count = jobGenerator(Steps)
+
+	options := &cagrr.Repair{
 		Keyspace: "testspace",
 		Cause:    "I can",
 		Owner:    "miller",
 		Callback: "http://localhost:8000/status",
 		Options: map[string]string{
 			"parallelism": "parallel",
-			"ranges":      "3432687330997893542:3451030515182791832",
 		},
 	}
-	buf, _ := json.Marshal(options)
-	body := bytes.NewBuffer(buf)
 
-	r, _ := http.Post("http://localhost:8080/repair", "application/json", body)
-	response, _ := ioutil.ReadAll(r.Body)
-	var repair Repair
-	err := json.Unmarshal(response, &repair)
-	if err == nil {
-		fmt.Println(repair)
+	ranges := getRanges()
+
+	for _, r := range ranges {
+		options.Options["ranges"] = r
+		buf, _ := json.Marshal(options)
+		body := bytes.NewBuffer(buf)
+
+		r, _ := http.Post("http://localhost:8080/repair", "application/json", body)
+		response, _ := ioutil.ReadAll(r.Body)
+		var repair Repair
+		err := json.Unmarshal(response, &repair)
+		if err == nil {
+			fmt.Println(repair)
+		} else {
+			fmt.Println("Error: ", err)
+		}
 	}
 
+}
+
+func main() {
+	flags.Parse(&opts)
+
+	go runServer()
+
+	node := cagrr.Node{Host: opts.Host, Port: opts.Port}
+
+	initLog(node)
+	initMetrics()
+
+	repair(node)
+
+}
+
+func initMetrics() {
+	addr, _ := net.ResolveTCPAddr("tcp", os.Getenv("GRAPHITE_URL"))
+	go graphite.Graphite(metrics.DefaultRegistry, 10e9, "cagrr", addr)
+	instr = Metrics{
+		fragmentCount: metrics.NewRegisteredCounter("fragmentCount", metrics.DefaultRegistry),
+		errorCount:    metrics.NewRegisteredCounter("errorCount", metrics.DefaultRegistry),
+		repairTime:    metrics.NewRegisteredMeter("repairTime", metrics.DefaultRegistry),
+	}
+}
+
+func initLog(node cagrr.Node) {
+	url := os.Getenv("ELASTICSEARCH_URL")
+	index := "cagrr"
+
+	client, err := elastic.NewClient(elastic.SetURL(url))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"url": url,
+		}).Panic(err)
+	}
+	level, _ := log.ParseLevel(opts.Verbosity)
+	hook, err := elogrus.NewElasticHook(client, node.Host, level, index)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"url":   url,
+			"index": index,
+		}).Panic(err)
+	}
+
+	logger := log.New()
+	logger.Hooks.Add(hook)
 }
