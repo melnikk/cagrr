409757e9991fc162ec2b48a0f49db59bd3d21ffd
Kirill Melnikov <kir@melniki.pro>
2017-02-17 22:33:56 +0500

    Table tests added

---
+++ table_test.go
@@ -0,0 +1,41 @@
+package cagrr_test
+
+import (
+	. "github.com/skbkontur/cagrr"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Table", func() {
+	var table *Table
+	BeforeEach(func() {
+		table = &Table{}
+	})
+	Context("totals", func() {
+		It("should be empty", func() {
+			Expect(table.Total()).To(Equal(0))
+		})
+
+		It("should set total", func() {
+			table.SetTotal(5)
+			Expect(table.Total()).To(Equal(5))
+		})
+
+	})
+	Context("repairs", func() {
+		It("should be nil", func() {
+			Expect(table.Repairs()).To(BeNil())
+		})
+
+		It("should set repairs", func() {
+			repair := &Repair{}
+			repairs := []*Repair{}
+			repairs = append(repairs, repair)
+			table.SetRepairs(repairs)
+
+			Expect(table.Repairs()[0]).To(BeIdenticalTo(repair))
+		})
+
+	})
+})
